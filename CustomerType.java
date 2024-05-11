9ea19f4fc06e40ba1709f10133f47e3a048d6fa6
Lidiia Kalugina <116164309+KLidya@users.noreply.github.com>
2024-05-11 15:22:42 -0500

    add CustomerType
    This program defines a Java enum named "CustomerType".
    This enum is used throughout the application to specify the type of customer for various operations or classifications.

---
+++ CustomerType.java
@@ -0,0 +1,9 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.itexps.bank_of_suburbs;
+
+public enum CustomerType {
+    Individual, Business
+}
